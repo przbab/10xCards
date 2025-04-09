e7774247223a1a026b2ab9a13cc533022d39e9b2
Przemysław Babiarz <przemyslaw.babiarz@schibsted.pl>
2025-04-09 20:37:36 +0200

    chore: module 1 lesson 2

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,9 +1,37 @@
 import { nicheBase, nichePrettier } from '@schibsted/niche-eslint-config';
+import eslintPluginAstro from 'eslint-plugin-astro';
 
 export default [
     ...nicheBase,
+    ...eslintPluginAstro.configs['flat/recommended'],
     ...nichePrettier,
     {
-        ignores: ['node_modules'],
+        settings: {
+            'import/parsers': {
+                '@typescript-eslint/parser': ['.ts', '.tsx', '.mjs'],
+                'astro-eslint-parser': ['.astro'],
+            },
+            'import/resolver': {
+                node: {
+                    extensions: ['.js', '.ts', '.mjs', '.astro'],
+                },
+                typescript: {
+                    project: './tsconfig.json',
+                },
+            },
+        },
+    },
+    {
+        ignores: ['dist', 'node_modules', '.astro', 'src/env.d.ts'],
+    },
+    {
+        rules: {
+            'import/extensions': [
+                'error',
+                {
+                    astro: 'always',
+                },
+            ],
+        },
     },
 ];
