805546ba5491116e0b9120a56b174a209905174b
Przemysław Babiarz <przemyslaw.babiarz@schibsted.pl>
2025-04-18 14:25:46 +0200

    fix: fix build

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,20 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+    adapter: node({
+        mode: 'standalone',
+    }),
+    experimental: {
+        session: true,
+    },
     integrations: [react()],
+
     output: 'server',
+
     vite: {
         plugins: [tailwindcss()],
     },
